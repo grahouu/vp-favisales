f1175878f0ac0d24d6ce739b1ae04ed201a0b22b
Alexis Collin <alexis-collin@hotmail.fr>
2017-12-04 22:06:31 +0100

    Add star o

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     return true;
 });
 
+let template_star_o = `<div class="fav_star" ope-id=""> <i class="fa fa-star-o fa-1" aria-hidden="true"></i> </div>`;
 let template_star = `<div class="fav_star" ope-id=""> <i class="fa fa-star fa-1" aria-hidden="true"></i> </div>`;
 let template_wrapper = `
 <div class="wrapperSales" id="favorites">
@@ -21,25 +22,31 @@ let favorites = JSON.parse(localStorage.getItem("favorites")) || {}
 
 function clickElement(myStar){
     myStar.click(function() {
-        let favorites_list_elem = $("#favorites_list")
         let star_elem = $(this)
         let opeId = star_elem.attr("ope-id")
-        let banner = $(".bannerWrapper[data-operation-id='"+opeId+"']").first()
-        if (banner.attr("fav") == "false"){
-            let newBanner = banner.clone();
+        let banners = $(".bannerWrapper[data-operation-id='"+opeId+"']")
+        if (banners.length > 0 && $(banners[0]).attr("fav") == "false"){
+            if (Object.keys(favorites).length == 0)
+                $( "#topcarousel" ).after(template_wrapper);
+            let newBanner = $(banners[0]).clone();
             newBanner.attr("fav", true);
             newBanner.css("display", "inline-block");
             newBanner.css("position", "");
             newBanner.css("transform", "");
             newBanner.css("margin-left", "20px");
             newBanner.css("margin-bottom", "20px");
+            newBanner.find(".fa-star-o").replaceWith('<i class="fa fa-star fa-1" aria-hidden="true"></i>')
+            $(banners[0]).find(".fa-star-o").replaceWith('<i class="fa fa-star fa-1" aria-hidden="true"></i>')
             clickElement(newBanner.find(".fav_star"));
-            newBanner.appendTo(favorites_list_elem);
             saveFav(opeId, newBanner)
-        }else if (banner.attr("fav") == "true"){
-            let idOpe = banner.attr("data-operation-id")
-            banner.remove();
+            newBanner.appendTo("#favorites_list");
+        }else if ($(banners[0]).attr("fav") == "true"){
+            let idOpe = $(banners[0]).attr("data-operation-id")
+            $($(".bannerWrapper[data-operation-id='"+opeId+"']")[1]).find(".fa-star").replaceWith('<i class="fa fa-star-o fa-1" aria-hidden="true"></i>')
+            $(banners[0]).remove();
             saveFav(opeId)
+            if (Object.keys(favorites).length == 0)
+                $("#favorites").remove();
         }
     });
 }
@@ -54,28 +61,34 @@ function saveFav(operationId, banner = null){
 }
 
 function setFavorites(){
-    let wrapper_elem = $("#favorites_list")
-
-    for (var property in favorites) {
-        if (favorites.hasOwnProperty(property)) {
-            let obj = $(favorites[property])
-            clickElement(obj.find(".fav_star"))
-            obj.appendTo(wrapper_elem);
+    if (Object.keys(favorites).length){
+        $( "#topcarousel" ).after(template_wrapper);
+        let wrapper_elem = $("#favorites_list")
+        for (var property in favorites) {
+            if (favorites.hasOwnProperty(property)) {
+                let obj = $(favorites[property])
+                clickElement(obj.find(".fav_star"))
+                obj.appendTo(wrapper_elem);
+            }
         }
     }
 }
 
 function addStar(elem){
     elem.attr("fav", false);
-    let obj_template_star = $(template_star)
-    obj_template_star.attr("ope-id", elem.attr("data-operation-id"))
+    let opId = elem.attr("data-operation-id")
+    let obj_template_star;
+    if (opId in favorites)
+        obj_template_star = $(template_star)
+    else
+        obj_template_star = $(template_star_o)
+    obj_template_star.attr("ope-id", opId)
     clickElement(obj_template_star);
     elem.find("#container").prepend(obj_template_star)
 }
 
 function start() {
     $('head').append('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">');
-    $( "#topcarousel" ).after(template_wrapper);
     $(".bannerWrapper").each(function () {
         addStar($(this))
         $(this).find(".LazyLoad").bind("DOMSubtreeModified",function(e){
